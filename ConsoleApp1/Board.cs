5f2d2d0c8511a68a3e41faec215ab7736b5d4c94
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-05-08 09:32:02 +0200

    Add files via upload

---
+++ ConsoleApp1/Board.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Board
+    {
+        private int[,] board;
+        private int width;
+        private int height;
+
+        public Board(int height, int width)
+        {
+            Width = width;
+            Height = height;
+            board = new int[height, width];
+        }
+
+        public int Width
+        {
+            set
+            {
+                this.width = value;
+            }
+            get
+            {
+                return width;
+            }
+        }
+
+        public int Height
+        {
+            set
+            {
+                this.height = value;
+            }
+            get
+            {
+                return height;
+            }
+        }
+
+        public int this[int row, int col]
+        {
+            get
+            {
+                return board[row, col];
+            }
+            set
+            {
+                board[row, col] = value;
+            }
+        }
+
+        public int GetLength(int dimension)
+        {
+            if(dimension == 0)
+            {
+                return height;
+            }
+            else if(dimension == 1)
+            {
+                return width;
+            }
+            else
+            {
+                throw new Exception("Wrong value for dimension");
+            }
+        }
+
+        public void Clear()
+        {
+            board = new int[height, width];
+        }
+
+        public bool IsRowComplete(int row)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                if (board[row, col] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void clearRow(int row)
+        {
+            for (int r = row; r > 0; r--)
+            {
+                for (int c = 0; c < width; c++)
+                {
+                    board[r, c] = board[r - 1, c];
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Shape.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Shape
+    {
+        private int[,] currentShape;
+        private int currentShapeRow;
+        private int currentShapeCol;
+
+        public Shape(int currentShapeRow, int currentShapeCol, int[,] currentShape)
+        {
+            CurrentShapeRow = currentShapeRow;
+            CurrentShapeCol = currentShapeCol;
+            CurrentShape = currentShape;
+        }
+
+        public int CurrentShapeRow
+        {
+            get
+            {
+                return currentShapeRow;
+            }
+            set 
+            {
+                this.currentShapeRow = value;
+            }
+        }
+
+        public int CurrentShapeCol
+        {
+            get
+            {
+                return currentShapeCol;
+            }
+            set
+            {
+                this.currentShapeCol = value;
+            }
+        }
+
+        public int [,] CurrentShape
+        {
+            get
+            {
+                return currentShape;
+            }
+            set
+            {
+                this.currentShape = value;
+            }
+        }
+
+        public int GetLength(int dimension)
+        {
+            if (currentShape == null)
+            {
+                throw new Exception("thisShape is null");
+            }
+
+            if (dimension == 0)
+            {
+                return currentShape.GetLength(0);
+            }
+            else if (dimension == 1)
+            {
+                return currentShape.GetLength(1);
+            }
+            else
+            {
+                throw new Exception("Wrong value for dimension");
+            }
+        }
+
+        public int this[int row, int col]
+        {
+            get
+            {
+                return currentShape[row, col];
+            }
+            set
+            {
+                currentShape[row, col] = value;
+            }
+        }
+    }
+}
